589a7a4268f151c7670846a0d3966572122870dd
Zhrins <resnaw182@gmail.com>
2025-10-05 10:58:20 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Nwalhost AI",
+  description: "Asisten AI buatan Nwalhost",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
